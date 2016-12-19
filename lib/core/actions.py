0ec9df434f7ef6d09a0ec434290feed84094fb38
hipnusleo <hipnusleo@outlook.com>
2016-12-19 21:47:15 +0800

    @2016-12-19 1. laserjet can run with option -exec -sync simultaneosly; 2. if local path == remote path on laserjet node, sync task will skip

---
+++ lib/core/actions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+"""
+    @Author:        yyg
+    @Create:        2016MMDD
+    @LastUpdate:    2016MMDD HH:MM:SS
+    @Version:       0.0
+    @Description    
+        #
+"""
